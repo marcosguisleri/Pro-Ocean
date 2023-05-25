b66ba7b85ab3f3ee388d2c0fb4c63ebf2940a721
Marcos Guisleri <81881706+marcosguisleri@users.noreply.github.com>
2023-05-25 10:17:39 -0300

    Versão final de apresentação
    Versão a ser apresentada como nota P2, hoje na aula de programação web.

---
+++ assets/js/galeria.js
@@ -0,0 +1,30 @@
+function mudarFotos() {
+
+    const novaFoto1 = document.getElementById("img1");
+    novaFoto1.setAttribute("src", "assets/img/tartaruga.jpg");
+
+    const novaFoto2 = document.getElementById("img2");
+    novaFoto2.setAttribute("src", "assets/img/ta.jpg");
+
+    const novaFoto3 = document.getElementById("img3");
+    novaFoto3.setAttribute("src", "assets/img/novo6.png");
+
+    const novaFoto4 = document.getElementById("img4");
+    novaFoto4.setAttribute("src", "assets/img/dia2.png");
+
+    const novaFoto5 = document.getElementById("img5");
+    novaFoto5.setAttribute("src", "assets/img/novo1.png");
+
+    const novaFoto6 = document.getElementById("img6");
+    novaFoto6.setAttribute("src", "assets/img/novo2.png");
+
+    const novaFoto7 = document.getElementById("img7");
+    novaFoto7.setAttribute("src", "assets/img/novo3.png");
+
+    const novaFoto8 = document.getElementById("img8");
+    novaFoto8.setAttribute("src", "assets/img/novo4.png");
+
+    const novaFoto9 = document.getElementById("img9");
+    novaFoto9.setAttribute("src", "assets/img/novo5.png");
+
+}
